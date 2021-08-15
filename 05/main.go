16ac63052149a24b8c2cd6b3d5670fb30acc2187
chenyunyun <chenyunyun@lianluo.com>
2021-08-15 22:58:53 +0800

    05week mod

--- 05/main.go
+++ 05/main.go
@@ -8,8 +8,8 @@ import (
 
 // SlidingWindowLimiter 滑动窗口计数器限流
 type SlidingWindowLimiter struct {
-	Interval    int64       // 总计数时间
-	LastTime    time.Time   // 上一个窗口时间
+	Interval    int64       // 总计数时间，*1000，转换为豪秒
+	LastTime    int64       // 上一个窗口时间,time.Now().UnixNano()
 	Lck         *sync.Mutex // 锁
 	WinCount    []int64     // 窗口中请求当前数量
 	TicketSize  int64       // 窗口最大容量
@@ -20,8 +20,8 @@ type SlidingWindowLimiter struct {
 // NewSlidingWindowLimiter 初始化滑动窗口计数器限流
 func NewSlidingWindowLimiter(interval int64, ticketCount int64, ticketSize int64) *SlidingWindowLimiter {
 	return &SlidingWindowLimiter{
-		Interval:    interval,
-		LastTime:    time.Now(),
+		Interval:    interval * 1000,
+		LastTime:    time.Now().UnixNano(),
 		TicketSize:  ticketSize,
 		TicketCount: ticketCount,
 		WinCount:    make([]int64, ticketCount, ticketCount),
@@ -36,10 +36,11 @@ func (r *SlidingWindowLimiter) slidingCounterLimit() bool {
 	defer r.Lck.Unlock()
 	// 一个窗口的时间段
 	eachTicketTime := r.Interval / r.TicketCount
-
-	now := time.Now()
+	// 当前时间
+	now := time.Now().UnixNano()
+	//fmt.Println(now/1e6,r.LastTime/1e6,now/1e6-r.LastTime/1e6,eachTicketTime)
 	// 如果间隔时间超过一个窗口的时间 当前窗口置0 指向下一个窗口
-	if now.Unix()-r.LastTime.Unix() > eachTicketTime {
+	if (now/1e6)-(r.LastTime/1e6) > eachTicketTime {
 		r.WinCount[r.CurIndex] = 0
 		r.CurIndex = (r.CurIndex + 1) % r.TicketCount
 		r.LastTime = now
@@ -57,6 +58,7 @@ func (r *SlidingWindowLimiter) slidingCounterLimit() bool {
 	return flag
 }
 func main() {
+	fmt.Println(time.Now().Unix())
 	//var wg sync.WaitGroup
 	// 定义1秒5个时间窗口 每个窗口大小为10  即1秒25个请求
 	r := NewSlidingWindowLimiter(1, 5, 5)
@@ -68,9 +70,10 @@ func main() {
 		} else {
 			fmt.Println("limit ", i)
 		}
-		time.Sleep(150 * time.Millisecond)
+		time.Sleep(30 * time.Millisecond)
 		//wg.Done()
 	}
-
 	//wg.Wait()
+
+	fmt.Println(time.Now().Unix())
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/google/wire v0.5.0
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 )
